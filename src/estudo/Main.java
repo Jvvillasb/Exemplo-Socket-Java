55a62e3d9023a6aa45c42cc27e663c35388af944
João Vitor Villas Bôas <jvvillasb@gmail.com>
2022-06-26 22:10:35 -0300

    Projetinho Comunicação via Socket

---
+++ src/estudo/Main.java
@@ -0,0 +1,24 @@
+package estudo;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(4000);
+        Socket socket = serverSocket.accept();
+        System.out.println("Cliente Conectou");
+
+        InputStreamReader inputReader = new InputStreamReader(socket.getInputStream());
+        PrintStream saida = new PrintStream(socket.getOutputStream());
+        BufferedReader reader = new BufferedReader(inputReader);
+        String x;
+        while((x = reader.readLine()) != null){
+            saida.println("Servidor: " + x);
+        }
+    }
+}
